897f032280a54ea2e193219d8dd29980c0edf4d1
Jonathan Silva <silvaj2020@fau.edu>
2021-11-13 17:12:27 -0500

    Move search results to results.php

--- backtogether/public/index.php
+++ backtogether/public/index.php
@@ -13,38 +13,11 @@
         require 'std_navbar.php';
         ?>
 
+        <!-- FRONTEND TEAM: Add some clip-art or something here and a few paragraphs explaining the site. -->
         <main>
-            <div class="container">
-                <div class="row">
-                    <div class="col-s-6">
-                    <?php
-                    if (isset($_POST['search']))
-                    {
-                        $search = "%" . $_POST['search'] . "%";
-                        $results = Database::Query("SELECT `type`,`first_name`,`last_name`,`location`,`profile_desc` FROM `users` WHERE `first_name` LIKE ? OR `last_name` LIKE ?;", $search, $search);
-
-                        if (!empty($results))
-                        {
-                            echo "<table class=\"table table-striped table-hover\">";
-                            echo "<thead><th scope=\"col\"> Name </th><th scope=\"col\"> Description </th></tr></thead><tbody>";
-
-                            foreach ($results as $row)
-                            {
-                                echo "<tr><td><div class=\"profile_name\">" . $row['first_name'] . " " . $row['last_name'] . "</div></td><td><div class=\"profile_desc\">" . $row['profile_desc'] . "</div></td></tr>";
-                            }
-                        }
-                        else
-                        {
-                            echo "<div class=\"alert alert-primary\" role=\"alert\"> No results found! </div>";
-                        }
-
-                        echo "</tbody>";
-                        echo "</table>";
-                    }
-                    ?>
-                    </div>
-                </div>
-            </div>
+            <article>
+                <h1>Welcome to BackTogether!</h1>
+            </article>
         </main>
     </body>
 </html>
