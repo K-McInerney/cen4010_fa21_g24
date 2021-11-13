8355d30515084a36daa8a5d7b962d123ad12e47a
Jonathan Silva <silvaj2020@fau.edu>
2021-11-13 17:12:40 -0500

    Fix navbar links

--- backtogether/public/std_navbar.php
+++ backtogether/public/std_navbar.php
@@ -24,13 +24,13 @@
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav mr-auto">
               <li class="nav-item active">
-                  <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                  <a class="nav-link" href="/index.php">Home <span class="sr-only">(current)</span></a>
               </li>
               <li class="nav-item">
-                  <a class="nav-link" href="#">Login</a>
+                  <a class="nav-link" href="/login.php">Login</a>
               </li>
           </ul>
-          <form action="index.php#search" method="post" class="form-inline my-2 my-lg-0">
+          <form action="results.php#search" method="post" class="form-inline my-2 my-lg-0">
               <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" name="search" id="search">
               <button class="btn btn-outline-success my-2 my-sm-0" type="submit" name="submit">Search</button>
           </form>
