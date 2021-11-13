0160aa519b165dbd4a8ee7294802a0f3b977dbc8
Jonathan Silva <silvaj2020@fau.edu>
2021-11-13 17:11:44 -0500

    Add login page frontend prototype

--- backtogether/public/login.php
+++ backtogether/public/login.php
@@ -14,3 +14,30 @@ require 'std_head.php';
     ?>
 
     <main>
+
+        <div class="wrapper fadeIn">
+            <div id="formContent">
+
+                <div>
+                    <img src="img/bt.svg" id="icon" alt="User Icon" />
+                </div>
+
+                <form action="login.php#trylogin">
+                    <input type="text" id="login" name="login" placeholder="username">
+                    <input type="text" id="password" name="login" placeholder="password">
+                    <input type="submit" value="Log In">
+                </form>
+
+                <form action="login.php#tryregister">
+                    <input type="text" id="register_username" name="register" placeholder="username">
+                    <input type="text" id="register_firstname" name="register" placeholder="first name">
+                    <input type="text" id="register_lastname" name="register" placeholder="last name">
+
+                    <input type="text" id="register_password" name="register" placeholder="password">
+                    <input type="text" id="register_confirmpassword" name="register" placeholder="confirm password">
+
+                    <input type="submit" value="Register">
+                </form>
+
+            </div>
+        </div>
