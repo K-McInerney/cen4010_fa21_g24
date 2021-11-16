5e8fa6f177b4e11a9e9bc6760cc971e105e42665
Jonathan Silva <silvaj2020@fau.edu>
2021-11-15 21:23:46 -0500

    Fix Foreign Key issue in schema.sql

--- backtogether/schema.sql
+++ backtogether/schema.sql
@@ -35,7 +35,7 @@ CREATE TABLE IF NOT EXISTS `posts` (
 
 -- Dumping structure for table cen4010_fa21_g24.users
 CREATE TABLE IF NOT EXISTS `users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(10) NOT NULL AUTO_INCREMENT,
   `type` tinyint(3) unsigned NOT NULL DEFAULT '2' COMMENT '0 - System, 1 - Event Host/Vendor, 2 - User',
   `username` varchar(50) DEFAULT NULL COMMENT 'Unique username',
   `first_name` varchar(50) NOT NULL COMMENT 'First Name or Company Name',
@@ -47,7 +47,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   UNIQUE KEY `username` (`username`)
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 
--- Dumping data for table cen4010_fa21_g24.users: ~3 rows (approximately)
+-- Dumping data for table cen4010_fa21_g24.users: ~5 rows (approximately)
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
 INSERT INTO `users` (`id`, `type`, `username`, `first_name`, `last_name`, `location`, `password_hash`, `profile_desc`) VALUES
 	(1, 2, 'aaaaaa', 'Test', 'Me', NULL, 'E53DC714E464628B0EB5D566A3B510805B4729CBED31D1106E016340070828475C102F5D0C2CD918F7128AD4C603585F66BFCB5A8AB36CE377E0EE018AD8FD1F', 'Hi!'),
