e191e5fadb27585a1d4b6ef76d061380ebf181f2
ThomasDMoraes <tmoraes2020@fau.edu>
2021-12-06 12:27:17 -0500

    Update index.php

--- backtogether/public/index.php
+++ backtogether/public/index.php
@@ -20,7 +20,78 @@
         <!-- FRONTEND TEAM: Add some clip-art or something here and a few paragraphs explaining the site. -->
         <main>
             <article>
-                <h1>Welcome to BackTogether!</h1>
+				<!-- Title -->
+                <h1 class="card-header">Welcome to BackTogether!</h1>
+				</br>
+
+				<!-- Carousel -->
+
+				<div class="carousel slide" id="myCarousel" data-ride="carousel">
+					<!-- Indicators -->
+					<ol class="carousel-indicators">
+						<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+						<li data-target="#myCarousel" data-slide-to="1"></li>
+						<li data-target="#myCarousel" data-slide-to="2"></li>
+					</ol>
+					<!-- Carousel content -->
+					<div class="carousel-inner bg-dark" align="center">
+						<!-- Slide 1 -->
+						<div class="carousel-item active">
+							<img src="https://www.no-copyright.com/static/preview2/stock-photo-stock-photo-no-copyright-image-no-copyright-stock-photo-122258.jpg" alt="concert" height="500" width="900">
+							<div class="carousel-caption">
+								<p>
+									Discover exciting new events near you
+								</p>
+							</div>
+						</div>
+						<!-- Slide 2 -->
+						<div class="carousel-item">
+						<img src="https://images.unsplash.com/photo-1532629345422-7515f3d16bb6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" alt="charity" height="500" width="900">
+						<div class="carousel-caption">
+								<p>
+									Become an active member in your community
+								</p>
+							</div>
+						</div>
+						<!-- Slide 3 -->
+						<div class="carousel-item">
+							<img src="https://images.pexels.com/photos/1023828/pexels-photo-1023828.jpeg?cs=srgb&dl=pexels-darrel-und-1023828.jpg&fm=jpg" alt="connections" height="500" width="900">
+								<div class="carousel-caption">
+								<p>
+									Meet people in your area
+								</p>
+							</div>
+						</div>
+					</div>
+
+					<!-- Carousel navigation buttons -->
+					<a class="carousel-control-prev" href="#myCarousel" data-slide="prev">
+						<span class="carousel-control-prev-icon"></span>
+						<span class="sr-only">Previous</span>
+					</a>
+					<a class="carousel-control-next" href="#myCarousel" data-slide="next">
+						<span class="carousel-control-next-icon"></span>
+						<span class="sr-only">Next</span>
+					</a>
+				</div>
+				<!-- end of carousel -->
+
+				</br>
+
+				<!-- Info section -->
+				<div class="card">
+					<!-- Info subheading -->
+					<h2 class="card-title">
+						Our Purpose
+					</h2>
+					<!-- Information paragraph -->
+					<p class="card-body wrapper">
+						BackTogether is a new social media website designed to help integrate people back into society as the threat of Covid-19 eventually subsides.
+						While hosts and representatives are encouraged to keep users updated on upcoming events, users are also encouraged to keep the community lively through their posts.
+						With upcoming events may range from wholesome small charities to extravagant live concerts, we aim to bring as many people as possible from all aspects of the community.
+						So what are you waiting for? Be a part of the community and help bring everyone back together.
+					</p>
+				</div>
             </article>
         </main>
     </body>
